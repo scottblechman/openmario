61ec7b5b370606b452b489dae8e60baaa6d4914d
Scott Blechman <14227901+scottblechman@users.noreply.github.com>
2020-07-26 16:05:41 -0500

    Initial commit

---
+++ desktop/src/com/scottblechman/openmario/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.scottblechman.openmario.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.scottblechman.openmario.OpenMario;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OpenMario(), config);
+	}
+}
