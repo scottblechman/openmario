dab6bc6407229e9e9b641ec5327130abebc46577
Scott Blechman <14227901+scottblechman@users.noreply.github.com>
2020-07-26 16:38:14 -0500

    Update LWJGL to version 3

--- desktop/src/com/scottblechman/openmario/desktop/DesktopLauncher.java
+++ desktop/src/com/scottblechman/openmario/desktop/DesktopLauncher.java
@@ -1,12 +1,13 @@
 package com.scottblechman.openmario.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.scottblechman.openmario.OpenMario;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new OpenMario(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("OpenMario");
+		new Lwjgl3Application(new OpenMario(), config);
 	}
 }
